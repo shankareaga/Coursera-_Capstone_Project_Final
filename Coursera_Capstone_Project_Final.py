428ab971cba5bc25aeb1eb65a04918ab18768f0e
shankareaga <69393623+shankareaga@users.noreply.github.com>
2020-10-07 18:51:41 +0100

    Add files via upload

---
+++ Coursera_Capstone_Project_Final.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Coursera Capstone project final - just to share Jupyter notebook
+
+# In[1]:
+
+
+import pandas as pd
+import numpy as np
+
+
+# In[3]:
+
+
+print("Hello Capstone Project Course!")
+
+
+# In[ ]:
+
+
+
+
